d7e56c20746357313b44924f6331f158fde7a398
mamatha-ravi <mamata.r@gmail.com>
2026-02-03 15:10:29 +0530

    Robo

--- common.sh
+++ common.sh
@@ -10,7 +10,7 @@ N="\e[0m"
 SCRIPT_DIR=$PWD
 START_TIME=$(date +%s)
 MONGODB_HOST=mongodb.devops88s.online
-MYSQL_HOST=mysql.devops88ss.online
+MYSQL_HOST=mysql.devops88s.online
 
 mkdir -p $LOGS_FOLDER
 
