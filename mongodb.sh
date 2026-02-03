ca26734916c7417d381795fb8b912bc52dc5dfc2
mamatha-ravi <mamata.r@gmail.com>
2026-02-03 13:27:34 +0530

    Robo

---
+++ mongodb.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+SCRIPT="mongodb"
+source ./common.sh
+Root_CHECK
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+validate "copying mongo repo"
+
+dnf install mongodb-org -y &>>$log_file
+validate "Installing Mongodb server"
+
+systemctl enable mongod 
+validate "enabling mongodb"
+systemctl start mongod 
+validate "starting mongodb"
+
+sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mongod.conf
+validate "allowing remote connections"
+
+systemctl restart mongod
+validate "restarted mongodb"
