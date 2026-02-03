f69298fa9278b83a5b8b0352c92006139467f317
mamatha-ravi <mamata.r@gmail.com>
2026-02-03 14:24:44 +0530

    Robo

---
+++ mysql.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+source ./common.sh
+
+app_name=mysql
+Root_CHECK
+
+dnf install mysql-server -y &>>$LOGS_FILE
+VALIDATE $? "Install MySQL server"
+
+systemctl enable mysqld &>>$LOGS_FILE
+systemctl start mysqld  
+VALIDATE $? "Enable and start mysql"
+
+# get the password from user
+mysql_secure_installation --set-root-pass RoboShop@1
+VALIDATE $? "Setup root password"
+
+TOTAL_EXE_TIME
